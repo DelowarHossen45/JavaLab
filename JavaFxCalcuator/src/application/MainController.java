ecb57fbd292fa8ee206f65957280824b68d0dff4
Delowar Hossen Apple <applecse17@gmail.com>
2018-11-24 21:17:23 +0600

    model create

--- JavaFxCalcuator/src/application/MainController.java
+++ JavaFxCalcuator/src/application/MainController.java
@@ -1,5 +1,18 @@
 package application;
 
-public class MainController {
+import java.awt.Desktop.Action;
+
+import javafx.event.ActionEvent;
+
+public class MainController 
+{
+	public void processNumber(ActionEvent event)
+	{
+		
+	}
+	public void processOperators(ActionEvent event)
+	{
+		
+	}
 
 }

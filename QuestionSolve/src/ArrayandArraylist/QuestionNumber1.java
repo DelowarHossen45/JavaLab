3a2f9e1046acdd3eb1fc119cb52d128d0da58f7c
Delowar Hossen Apple <applecse17@gmail.com>
2018-11-29 08:25:11 +0600

    b number complete

--- QuestionSolve/src/ArrayandArraylist/QuestionNumber1.java
+++ QuestionSolve/src/ArrayandArraylist/QuestionNumber1.java
@@ -5,15 +5,26 @@ public class QuestionNumber1
 	public static void main(String arg[])
 	{
 		//int a[]=new int[10];
+		//a number complete
 		int a[]= {1,2,3,4,5,4,3,2,1,0};
-		 int i;
+		int i;
 		for(i=1;i<10;i++)
 		{
 			a[i]=a[i-1];
 		}
 		for(i=1;i<10;i++)
 		{
-			System.out.print(a[i]+" ");
+			System.out.print(a[i]+ " ");
+		}
+		System.out.println();
+		int b[]= {1,2,3,4,5,4,3,2,1,0};
+		for(i=9;i>0;i--)
+		{
+			b[i]=b[i-1];
+		}
+		for(i=1;i<10;i++)
+		{
+			System.out.print(b[i]+ " ");
 		}
 	}
 

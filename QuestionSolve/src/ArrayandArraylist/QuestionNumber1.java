700cfeb38437c5308c69728d62f9455a368f1743
Delowar Hossen Apple <applecse17@gmail.com>
2018-11-29 08:22:36 +0600

    a number complete

--- QuestionSolve/src/ArrayandArraylist/QuestionNumber1.java
+++ QuestionSolve/src/ArrayandArraylist/QuestionNumber1.java
@@ -1,5 +1,20 @@
 package ArrayandArraylist;
 
-public class QuestionNumber1 {
+public class QuestionNumber1
+{
+	public static void main(String arg[])
+	{
+		//int a[]=new int[10];
+		int a[]= {1,2,3,4,5,4,3,2,1,0};
+		 int i;
+		for(i=1;i<10;i++)
+		{
+			a[i]=a[i-1];
+		}
+		for(i=1;i<10;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
 
 }
